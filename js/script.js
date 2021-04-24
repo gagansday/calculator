43f971b60658736b395d1c3a585bf1f6687b73eb
Gagandeep Singh <gagansday@gmail.com>
2021-04-24 16:52:52 +0530

    togglePlusMinus and backspace button

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
-const buttons = document.querySelector('.calculator__buttons');
+(function () {
+  const buttons = document.querySelector('.calculator__buttons');
+  const mainDisplayBox = document.querySelector('.calculator__display--text');
+  let input = '';
 
-buttons.addEventListener('click', e => {
-  const action = e.target.dataset.action;
+  function backspace() {
+    if (input) input = input.slice(0, -1).trim();
+  }
 
-  console.log(action);
-});
+  function togglePlusMinus() {
+    if (input.slice(0, 1) === '-') {
+      input = input.slice(1);
+    } else {
+      input = '-' + input;
+    }
+  }
+
+  function addToInput(action) {
+    input += action;
+  }
+
+  buttons.addEventListener('click', e => {
+    const action = e.target.dataset.action;
+
+    switch (action) {
+      case 'backspace':
+        backspace();
+        break;
+      case 'toggle-plus-minus':
+        togglePlusMinus();
+        break;
+      default:
+        addToInput(action);
+        break;
+    }
+
+    console.log(input);
+    mainDisplayBox.innerHTML = input;
+  });
+})();
